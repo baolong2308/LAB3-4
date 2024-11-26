bff311a6fc1d1b22d5bf7dad71d1e4d86de0ec47
BaoLong <phanhabaolong@gmail.com>
2024-11-26 22:12:41 +0700

    update

--- STM32/Core/Src/fsm_manual.c
+++ STM32/Core/Src/fsm_manual.c
@@ -1,115 +1,108 @@
 /*
- * fsm_manual.c
+ * fsm_setting.c
  *
  *  Created on: Oct 27, 2024
  *      Author: admin
  */
-
 #include"fsm_manual.h"
 
 void fsm_manual_run() {
 	switch (status) {
+	case MANUAL:
 
-	case SET_RED:
+		switch (status_1) {
+		case MAN_RED_1:
+			turnOn_RED1();
+			if (isButtonPressed(1) == 1) {
+				status_1 = MAN_YELLOW_1;
+			}
+			if (isTimerExpired(3) == 1) {
 
+				scanLEDs(99, 5);
+			}
+			if (isButtonPressed(0) == 1) {
+				status = READY;
+			}
+			break;
+		case MAN_YELLOW_1:
+			turnOn_YELLOW1();
+			if (isButtonPressed(1) == 1) {
+				status_1 = MAN_GREEN_1;
+			}
+			if (isButtonPressed(0) == 1) {
+				status = READY;
+			}
+			if (isTimerExpired(3) == 1) {
 
-		if (isTimerExpired(6) == 1) {
-			HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
-			HAL_GPIO_TogglePin(LED_4_GPIO_Port, LED_4_Pin);
-			setTimer(6, 50);
+				scanLEDs(99, 5);
+			}
+			break;
+		case MAN_GREEN_1:
+			turnOn_GREEN1();
+			if (isButtonPressed(1) == 1) {
+				status_1 = MAN_RED_1;
+			}
+			if (isButtonPressed(0) == 1) {
+				status = READY;
+			}
+			if (isTimerExpired(3) == 1) {
+
+				scanLEDs(99, 5);
+			}
 		}
 
-		if (isButtonPressed(0) == 1) {
-			status = SET_YELLOW;
-			turnOff();
-			count = 0;
+		switch (status_2) {
+		case MAN_RED_2:
+			turnOn_RED2();
+			if (isButtonPressed(2) == 1) {
+				status_2 = MAN_YELLOW_2;
+			}
+			if (isTimerExpired(3) == 1) {
 
-		}
-		if (isTimerExpired(3) == 1) {
-
-			scanLEDs(count, 2);
-		}
-		if (isButtonPressed(1) == 1) {
-			count++;
-
-		}
-		if (isButtonPressed(2) == 1) {
-			TIME_RED = count;
-			time_1 = count;
-		}
-		break;
-	case SET_GREEN:
-
-
-		if (isTimerExpired(6) == 1) {
-			HAL_GPIO_TogglePin(LED_6_GPIO_Port, LED_6_Pin);
-			HAL_GPIO_TogglePin(LED_3_GPIO_Port, LED_3_Pin);
-			setTimer(6, 50);
-		}
-
-		if (isTimerExpired(3) == 1) {
-			scanLEDs(count, 4);
-		}
-
-		if (isButtonPressed(1) == 1) {
-			count++;
-
-		}
-		if (isButtonPressed(2) == 1) {
-			TIME_GREEN = count;
-			time_1 = count;
-		}
-		if (isButtonPressed(0) == 1) {
-			count = 0;
-			if (TIME_RED == TIME_YELLOW + TIME_GREEN) {
-				status = RED_GREEN;
-				time_1 = TIME_GREEN - 1;
-				time_2 = TIME_RED - 1;
-				setTimer(2, 100);
-				setTimer(1, TIME_GREEN * 100);
-
-			} else {
-				status = ERR;
-				setTimer(4, 100);
+				scanLEDs(99, 5);
+			}
+			if (isButtonPressed(0) == 1) {
+				status = READY;
+			}
+			break;
+		case MAN_YELLOW_2:
+			turnOn_YELLOW2();
+			if (isButtonPressed(2) == 1) {
+				status_2 = MAN_GREEN_2;
 			}
 
+			if (isTimerExpired(3) == 1) {
+
+				scanLEDs(99, 5);
+			}
+			if (isButtonPressed(0) == 1) {
+				status = READY;
+			}
+			break;
+		case MAN_GREEN_2:
+			turnOn_GREEN2();
+			if (isButtonPressed(2) == 1) {
+				status_2 = MAN_RED_2;
+			}
+			if (isTimerExpired(3) == 1) {
+
+				scanLEDs(99, 5);
+			}
+			if (isButtonPressed(0) == 1) {
+				status = READY;
+			}
+			break;
 		}
+
 		break;
-	case SET_YELLOW:
+	case READY:
 
+		status = RED_GREEN;
+		time_1 = TIME_GREEN - 1;
+		time_2 = TIME_RED - 1;
+		setTimer(2, 100);
+		setTimer(1, TIME_GREEN * 100);
 
-		if (isTimerExpired(6) == 1) {
-			HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);
-			HAL_GPIO_TogglePin(LED_5_GPIO_Port, LED_5_Pin);
-			setTimer(6, 50);
-		}
-
-		if (isTimerExpired(3) == 1) {
-
-			scanLEDs(count, 3);
-
-		}
-		if (isButtonPressed(0) == 1) {
-			status = SET_GREEN;
-			turnOff();
-			count = 0;
-		}
-		if (isButtonPressed(1) == 1) {
-			count++;
-
-		}
-		if (isButtonPressed(2) == 1) {
-			TIME_YELLOW = count;
-
-		}
-		break;
-	case ERR:
-		if (isTimerExpired(3)) {
-			scanLEDs(99, 99);
-		}
-		if (isTimerExpired(4)) {
-			status = INIT;
-		}
 		break;
 	default:
 		break;
